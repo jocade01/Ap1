151ad8479fc9201dd3deb97f97c40077428806f2
JOSE CANTERO DEL COSO <jocade01@alumnatflorida.es>
2025-09-11 11:49:15 +0200

    ap3

---
+++ Bloque_1/01-Refuerzo_PHP/Actividades/AP1-3/AP1-3.php
@@ -0,0 +1,36 @@
+<?php
+
+$array = [1 => "primero",
+    3 => "segundo",
+    5 => "tercero",
+    7 => "cuarto",
+    9 => "quinto",
+    11 => "sexto"];
+
+$suma = 0;
+foreach ($array as $key => $value) {
+if ($value === "primero" ||  $value === "tercero" || $value === "quinto") {
+    echo "Estas en una posicion impar <br>";
+    $impar = true;
+    $par = false;
+    $suma = $suma + $key;
+
+} else if ($value === "segundo" || $value === "cuarto" || $value === "sexto"){
+    echo "Estas en una posicion par <br>";
+    $impar = false;
+    $par = true;
+    $suma = $suma + $key;
+}
+if ($suma < 5){
+    echo "El valor es menor que 5 <br>";
+} else if ($suma > 5 && $suma < 10) {
+    echo "El valor es mayor que 5 <br>";
+} else if ($suma > 10 && $suma < 20) {
+    echo "El valor es mayor que 10 <br>";
+
+} else if ($suma > 20 && $suma < 100) {
+    echo "El valor es mayor que 20 <br>";
+}
+}
+
+?>
