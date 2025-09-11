13c4ee6ff1fd291341dcf4f95d92b0d9077214dd
JOSE CANTERO DEL COSO <jocade01@alumnatflorida.es>
2025-09-11 11:30:34 +0200

    ap1

--- Bloque_1/01-Refuerzo_PHP/Actividades/AP1-1/AP1-1.php
+++ Bloque_1/01-Refuerzo_PHP/Actividades/AP1-1/AP1-1.php
@@ -1,4 +1,10 @@
 <?php
-echo 'hola'
-?>
+$array = [];
 
+foreach ($_GET as $key => $value) {
+    $array[$key] = $value;
+}
+foreach ($array as $key => $value) {
+    echo "Se ha recibido " . $value ." para la clave  $key <br>";
+}
+?>
